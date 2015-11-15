9977ee3dae830e5e38144f7e1085f5bb0dc6dbfc
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-15 12:42:48 +0100

    Another implementation of  Game-of-Threes

--- src/clojure_exercises/answers/game_of_threes.clj
+++ src/clojure_exercises/answers/game_of_threes.clj
@@ -9,3 +9,15 @@
         0 (recur (/ i 3) (conj acc [i 0]))
         1 (recur (dec i) (conj acc [i -1]))
         2 (recur (inc i) (conj acc [i 1]))))))
+
+(defn unfold [z f]
+  (lazy-seq
+    (when-let [[x s] (f z)]
+      (cons x (unfold s f)))))
+
+(defn game-of-threes2 [n]
+  (unfold n #(when-not (<= % 1)
+              (case (rem % 3)
+                0 [[% 0] (/ % 3)]
+                1 [[% -1] (dec %)]
+                2 [[% 1] (inc %)]))))
--- test/clojure_exercises/answers/game_of_threes_test.clj
+++ test/clojure_exercises/answers/game_of_threes_test.clj
@@ -1,8 +1,18 @@
 (ns clojure-exercises.answers.game-of-threes-test
-  (:require [midje.sweet :refer :all]
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
             [clojure-exercises.answers.game-of-threes :refer :all]))
 
-(facts "about game-of-threes"
-  (game-of-threes -100) => []
-  (game-of-threes 1) => []
-  (game-of-threes 100) => [[100 -1] [99 0] [33 0] [11 1] [12 0] [4 -1] [3 0]])
+(defn test-game-of-threes [f]
+  (is (= [] (f -100)))
+  (is (= [] (f 1)))
+  (is (= [[100 -1] [99 0] [33 0] [11 1] [12 0] [4 -1] [3 0]] (f 100))))
+
+(deftest can-game-of-threes
+  (test-game-of-threes game-of-threes)
+  (test-game-of-threes game-of-threes2))
+
+(facts "about unfold"
+  (take 5 (unfold 1 #(vector % (inc %)))) => [1 2 3 4 5]
+  (unfold 1 #(when-not (= % 3)
+              [% (inc %)])) => [1 2])
