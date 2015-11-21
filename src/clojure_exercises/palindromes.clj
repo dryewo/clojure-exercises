0b09ce3ce0336b559ff633e8d388cb79bfebc5f9
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-21 15:41:26 +0100

    Refactor Palindromes

--- src/clojure_exercises/palindromes.clj
+++ src/clojure_exercises/palindromes.clj
@@ -1,4 +1,14 @@
-(ns clojure-exercises.palindromes)
+(ns clojure-exercises.palindromes
+  (:require [midje.sweet :refer :all]))
 
 (defn palindrome? [text]
   )
+
+(future-facts
+  "about palindrome?"
+  (palindrome? "Are we not drawn onward, \nwe few, drawn onward to new area?")
+  => false
+  (palindrome? "A man, a plan, \na canal, a hedgehog, \na podiatrist, \nPanama!")
+  => false
+  (palindrome? "Was it a car\nor a cat\nI saw?")
+  => true)
