3e80ae40777a088fcba1c23bac9c36d0db4a7f87
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-21 15:46:21 +0100

    Refactor Typoglycemia

--- src/clojure_exercises/answers/typoglycemia.clj
+++ src/clojure_exercises/answers/typoglycemia.clj
@@ -1,5 +1,6 @@
 (ns clojure-exercises.answers.typoglycemia
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s]
+            [midje.sweet :refer :all]))
 
 (defn letter? [^Character c]
   (Character/isLetter c))
@@ -36,3 +37,57 @@
                             w))]
     (apply str processed-words)))
 
+;; TESTS
+
+(facts "about split-word"
+  (fact "behaves reasonably on non-conforming input"
+    (split-word "") => [[] [] []]
+    (split-word "a") => [[\a] [] []]
+    (split-word "ab") => [[\a] [] [\b]])
+  (fact "does what it's supposed to"
+    (split-word "abaca") => [[\a] [\b \a \c] [\a]]))
+
+(facts "about processable-word?"
+  (processable-word? "") => false
+  (processable-word? "a") => false
+  (processable-word? "ab") => false
+  (processable-word? "abc") => false
+  (processable-word? "abca") => true
+  (processable-word? "1234") => false)
+
+(facts "about process-word"
+  (fact "can shuffle"
+    (process-word "more") => "mroe"
+    (provided
+      (shuffle [\o \r]) => [\r \o])))
+
+(defn typoglycemic? [original-text re]
+  (fn [actual-text]
+    (boolean (and (not= original-text actual-text)
+                  (re-seq re actual-text)))))
+
+(facts "about typoglycemize"
+  (fact "in some cases it does not change"
+    (typoglycemize nil) => ""
+    (typoglycemize "") => ""
+    (typoglycemize "1234") => "1234"
+    (typoglycemize "a") => "a"
+    (typoglycemize "are") => "are"
+    (typoglycemize "Big Ben") => "Big Ben")
+  (fact "works on a single word"
+    (typoglycemize "I am better") => "I am bteetr"
+    (provided
+      (process-word "better") => "bteetr"))
+  (fact "works on sentences"
+    (typoglycemize "Surely You're Joking, Mr. Feynman!") => anything
+    (provided
+      (process-word "Surely") => ""
+      (process-word "Joking") => ""
+      (process-word "Feynman") => ""))
+  (fact
+    (typoglycemize "Formation")
+    => (typoglycemic? "Formation"
+                      #"^F[ormatio]{7}n$")
+    (typoglycemize "Surely You're Joking, Mr. Feynman!")
+    => (typoglycemic? "Surely You're Joking, Mr. Feynman!"
+                      #"^S[urel]{4}y You're J[okin]{4}g, Mr. F[eynma]{5}n!$")))
--- src/clojure_exercises/typoglycemia.clj
+++ src/clojure_exercises/typoglycemia.clj
@@ -1,8 +1,25 @@
 (ns clojure-exercises.typoglycemia
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s]
+            [midje.sweet :refer :all]
+            [clojure-exercises.answers.typoglycemia :refer [typoglycemic?]]))
 
 ;; https://www.reddit.com/r/dailyprogrammer/comments/3s4nyq
 
 (s/defn typoglycemize :- s/Str
   [text :- (s/maybe s/Str)]
   "")
+
+(future-facts
+  "about typoglycemize"
+  (fact "in some cases it does not change"
+    (typoglycemize "") => ""
+    (typoglycemize "1234") => "1234"
+    (typoglycemize "a") => "a"
+    (typoglycemize "are") => "are")
+  (fact
+    (typoglycemize "Formation")
+    => (typoglycemic? "Formation"
+                      #"^F[ormatio]{7}n$")
+    (typoglycemize "Surely You're Joking, Mr. Feynman!")
+    => (typoglycemic? "Surely You're Joking, Mr. Feynman!"
+                      #"^S[urel]{4}y You're J[okin]{4}g, Mr. F[eynma]{5}n!$")))
