f8a8d66a6f3b4f13f725264060f695c0e1f3875e
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-21 15:35:41 +0100

    Refactor Number Maze

--- src/clojure_exercises/answers/number_maze.clj
+++ src/clojure_exercises/answers/number_maze.clj
@@ -1,4 +1,5 @@
-(ns clojure-exercises.answers.number-maze)
+(ns clojure-exercises.answers.number-maze
+  (:require [midje.sweet :refer :all]))
 
 (def OPS [#(if (even? %) (/ % 2) nil)
           #(* % 2)
@@ -18,3 +19,17 @@
     (if-let [res (some #(when (= end (last %)) %) paths)]
       res
       (recur (mapcat #(step OPS %) paths)))))
+
+(facts "about step"
+  (step OPS [1]) => [[1 2] [1 3]]
+  (step OPS [2]) => [[2 1] [2 4]]
+  (step OPS [2 4]) => [[2 4 8] [2 4 6]]
+  (step OPS [6 3]) => [[6 3 5]])
+
+(facts "about solve"
+  (solve 1 1) => [1]
+  (solve 3 12) => [3 6 12]
+  (solve 12 3) => [12 6 3]
+  (solve 5 9) => [5 7 9]
+  (solve 9 2) => [9 18 20 10 12 6 8 4 2]
+  (solve 9 12) => [9 18 20 10 12])
--- src/clojure_exercises/number_maze.clj
+++ src/clojure_exercises/number_maze.clj
@@ -1,6 +1,15 @@
-(ns clojure-exercises.number-maze)
+(ns clojure-exercises.number-maze
+  (:require [midje.sweet :refer :all]))
 
 ;; https://www.4clojure.com/problem/106
 
 (defn solve [start end]
   )
+
+(future-facts "about solve"
+              (count (solve 1 1)) => 1
+              (count (solve 3 12)) => 3
+              (count (solve 12 3)) => 3
+              (count (solve 5 9)) => 3
+              (count (solve 9 2)) => 9
+              (count (solve 9 12)) => 5)
