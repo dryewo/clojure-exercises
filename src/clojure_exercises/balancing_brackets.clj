3db942e2d83dfaad26e1a8d59900a4b122135394
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-25 17:40:07 +0100

    Workshop 2015-11-25

--- src/clojure_exercises/balancing_brackets.clj
+++ src/clojure_exercises/balancing_brackets.clj
@@ -3,19 +3,38 @@
 
 ;; https://www.4clojure.com/problem/177
 
-(defn balanced? [text]
-  )
+;; ( { ) }
 
-(future-facts "about balanced?"
-              (balanced? "This string has no brackets.") => true
-              (balanced? "class Test {
+(defn spy [x]
+  (prn x)
+  x)
+
+(defn balanced? [text]
+  (let [closing? {\) \(
+                  \] \[
+                  \} \{}
+        opening? (set (vals closing?))]
+    (empty? (spy (reduce (fn [acc c]
+                           (cond
+                             (opening? c) (conj acc c)
+                             (closing? c) (if (= (last acc) (closing? c))
+                                            (pop acc)
+                                            (reduced [:error acc c]))
+                             :else acc))
+                         [] text)))))
+
+
+
+(facts "about balanced?"
+  (balanced? "This string has no brackets.") => true
+  (balanced? "class Test {
       public static void main(String[] args) {
         System.out.println(\"Hello world.\");
       }
     }") => true
-              (balanced? "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))") => true
-              (balanced? "(start, end]") => false
-              (balanced? "())") => false
-              (balanced? "[ { ] } ") => false
-              (balanced? "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))") => false
-              (balanced? "[") => false)
+  (balanced? "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))") => true
+  (balanced? "(start, end]") => false
+  (balanced? "())") => false
+  (balanced? "[ { ] } ") => false
+  (balanced? "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))") => false
+  (balanced? "[") => false)
--- src/clojure_exercises/happy_numbers.clj
+++ src/clojure_exercises/happy_numbers.clj
@@ -3,9 +3,83 @@
 
 ;; https://en.wikipedia.org/wiki/Happy_number
 
-(defn happy? [n]
-  true)
+;; 13
+;; 1*1 + 3*3 = 10
+;; 1*1 + 0*0 = 1
 
-(future-facts "about happy?"
-              (filter happy? (range 100))
-              => [1 7 10 13 19 23 28 31 32 44 49 68 70 79 82 86 91 94 97])
+;; 6
+;; 36
+;; 9 + 36 = 45
+;; 16 + 25 = 41
+;; 16 + 1 = 17
+;;
+
+;; 44
+;; 16 + 16 = 32
+;; 9 + 4 = 13
+;; 1 + 9 = 10
+;; 1
+
+;; 43
+;; 16 + 9 = 25
+;; 4 + 25 = 29
+;; 4 + 81 = 85
+;; 64 + 25 = 89
+;; 64 + 81 = 155
+;; 1 + 25 + 25 = 51
+;; 25 + 1 = 26
+;; 4 + 36 = 40
+;; 16
+;; 1 + 36 = 37
+;; 9 + 49 = 58
+;; 25 + 64 = 89
+
+(defn sqr [n]
+  (* n n))
+
+(defn number->digits1 [n]
+  (->> n
+       str
+       (map str)
+       (map #(Integer/valueOf ^String %))))
+
+(defn number->digits2 [n]
+  (loop [n n
+         acc []]
+    (if (zero? n)
+      (rseq acc)
+      (let [q (quot n 10)
+            r (rem n 10)]
+        (recur q (conj acc r))))))
+
+(facts
+  (number->digits1 43) => [4 3])
+
+(facts
+  (number->digits2 43) => [4 3])
+
+(defn happy-step [n]
+  (->> n
+       number->digits1
+       (map sqr)
+       (apply +)))
+
+(facts "about happy-step"
+  (happy-step 0) => 0
+  (happy-step 1) => 1
+  (happy-step 45) => 41)
+
+(defn happy? [n]
+  (loop [n n
+         acc #{}]
+    (cond
+      (= n 1) true
+      (acc n) false
+      :else (recur (happy-step n) (conj acc n)))))
+
+(facts "about happy?"
+  (happy? 1) => true)
+
+(facts "about happy?"
+  (filter happy? (range 100))
+  => [1 7 10 13 19 23 28 31 32 44 49 68 70 79 82 86 91 94 97])
