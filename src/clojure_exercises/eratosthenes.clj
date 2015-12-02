dc7af3a502be05ac52fdfa90789b1bd382bb50fb
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-12-02 22:46:00 +0100

    Workshop 2015-12-02

--- src/clojure_exercises/eratosthenes.clj
+++ src/clojure_exercises/eratosthenes.clj
@@ -3,8 +3,42 @@
 
 ;; https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
-(defn primes-up-to [n]
-  [])
+(defn divisible? [n q]
+  (zero? (rem n q)))
 
-(future-facts "about primes-up-to"
-              (primes-up-to 100) => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97])
+(facts "about divisible?"
+  (divisible? 9 2) => false
+  (divisible? 8 4) => true)
+
+(defn sieve [coll m]
+  (into [] (remove #(divisible? % m) coll)))
+
+(facts "about sieve"
+  (sieve [2 3 4 5 6 7] 2) => [3 5 7]
+  (sieve [2 3 4 5 6 7] 3) => [2 4 5 7])
+
+(defn primes-up-to [n]
+  (loop [[m & tail] (range 2 (inc n))
+         r []]
+    (if-not m
+      r
+      (recur (sieve tail m) (conj r m)))))
+
+(defn divisible-by-any? [n coll]
+  (boolean (some #(divisible? n %) coll)))
+
+(fact "about divisible-by-any?"
+  (divisible-by-any? 10 [2 3 5 7]) => true
+  (divisible-by-any? 11 [2 3 5 7]) => false)
+
+(defn primes-up-to2 [n]
+  (reduce (fn [acc x]
+            (if (divisible-by-any? x acc)
+              acc
+              (conj acc x)))
+          [] (range 2 (inc n))))
+
+(facts "about primes-up-to"
+  (primes-up-to 100) => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97])
+(facts "about primes-up-to2"
+  (primes-up-to2 100) => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97])
--- src/clojure_exercises/palindromes.clj
+++ src/clojure_exercises/palindromes.clj
@@ -1,13 +1,40 @@
 (ns clojure-exercises.palindromes
-  (:require [midje.sweet :refer :all]))
+  (:require [midje.sweet :refer :all]
+            [clojure.string :as str]))
 
-(defn palindrome? [text]
+(defn normalize [text]
+  (->> text
+       (filter #(Character/isLetter ^Character %))
+       (map #(Character/toLowerCase ^Character %))))
+
+(defn normalize2 [text]
+  (sequence
+    (comp
+      (filter #(Character/isLetter ^Character %))
+      (map #(Character/toLowerCase ^Character %)))
+    text))
+
+(facts "about normalize"
+  (normalize "Was it a car\nor a cat\nI saw?")
+  => (seq "wasitacaroracatisaw")
+  (normalize2 "Was it a car\nor a cat\nI saw?")
+  => (seq "wasitacaroracatisaw")
   )
 
-(future-facts
+(defn palindrome? [text]
+  (let [normalized-text (normalize text)]
+    (= normalized-text (reverse normalized-text))))
+
+(defn palindrome2? [text]
+  (let [normalized-text (normalize2 text)]
+    (= normalized-text (reverse normalized-text))))
+
+(facts
   "about palindrome?"
   (palindrome? "Are we not drawn onward, \nwe few, drawn onward to new area?")
   => false
+  (palindrome? "А роза упала на лапу Азора.")
+  => true
   (palindrome? "A man, a plan, \na canal, a hedgehog, \na podiatrist, \nPanama!")
   => false
   (palindrome? "Was it a car\nor a cat\nI saw?")
