179f0c01449e1cdc1a09d27f0d0a2e996d92a2a6
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-03-20 11:37:11 +0100

    8 Queens solution cleaned up

--- src/clojure_exercises/answers/8queens.clj
+++ src/clojure_exercises/answers/8queens.clj
@@ -1,69 +1,5 @@
 (ns clojure-exercises.answers.8queens
-  (:require [midje.sweet :refer :all])
-  (:import (clojure.lang PersistentQueue)))
-
-;; board: [[row col] ...]
-
-;(defn bounds-ok? [size board]
-;  (every? #(<= 1 % size) (flatten board)))
-;
-;(defn count-ok? [size board]
-;  (= size (count board)))
-;
-;(defn same-row? [size board]
-;  (= size (count (set (map first board)))))
-;
-;(defn same-column? [size board]
-;  (= size (count (set (map second board)))))
-;
-;;; [1 1] [2 2] [3 3]
-;;; [1 2] [2 3] [3 4]
-;;; [1 3] [2 2] [3 1]
-;
-;;; r c -> r - N + c
-;
-;(defn same-diagonal? [size board]
-;  (and (= size (count (set (map (partial apply -) board))))
-;       (= size (count (set (map (partial apply #(- %1 (- size %2))) board))))))
-;
-;(facts ""
-;  (bounds-ok? 2 [[1 1] [2 2]]) => true
-;  (bounds-ok? 2 [[1 1] [2 3]]) => false
-;  (bounds-ok? 2 [[1 1] [2 0]]) => false
-;  (count-ok? 2 [[1 1] [2 2]]) => true
-;  (count-ok? 2 [[1 1]]) => false
-;  (same-row? 2 [[1 2] [2 1]]) => true
-;  (same-row? 2 [[1 1] [1 2]]) => false
-;  (same-column? 2 [[1 2] [2 1]]) => true
-;  (same-column? 2 [[1 1] [2 1]]) => false
-;  (same-diagonal? 2 [[1 1] [2 2]]) => false
-;  (same-diagonal? 2 [[1 2] [2 1]]) => false
-;  (same-diagonal? 2 [[1 1] [1 2]]) => true)
-;
-;(defn check [size board]
-;  (let [brd (set board)]
-;    (and (count-ok? size brd)
-;         (bounds-ok? size brd)
-;         (same-row? size brd)
-;         (same-column? size brd)
-;         (same-diagonal? size brd))))
-;
-;(defn board->str [size board]
-;  (clojure.string/join
-;    "\n" (for [r (range 1 (inc size))]
-;           (clojure.string/join
-;             " " (for [c (range 1 (inc size))]
-;                   (if (contains? (set board) [r c])
-;                     "*"
-;                     "-"))))))
-;
-;(comment
-;  (println (board->str 2 [[1 1] [1 2]]))
-;  )
-;
-;(defn queens-impl [size acc]
-;  (if (= size (count acc))
-;    acc))
+  (:require [midje.sweet :refer :all]))
 
 ;; Adapted after https://github.com/jeffbrown/clojure-n-queens
 
@@ -87,28 +23,9 @@
 (defn solve-for-board-size [board-size]
   (nth (iterate #(add-to-boards % board-size) [[]]) board-size))
 
+(facts
+  (count (solve-for-board-size 8)) => 92)
+
 (comment
   (time (count (solve-for-board-size 10)))
   )
-
-;(defn queens [size]
-;  (let [solutions (atom [])
-;        impl-fn (fn impl [board]
-;                  (if (= size (count board))
-;                    (when (check size board)
-;                      (swap! solutions conj board))
-;                    (let [next-row (count board)]
-;                      )))]
-;    (impl-fn [])))
-
-;(defn queens [size]
-;  (loop [queue PersistentQueue/EMPTY
-;         row 1
-;         results []]
-;    (if (> row size)
-;      results
-;      (let []
-;        (recur (conj queue new-work) (inc row) results)))))
-
-;(facts "queens"
-;  )
