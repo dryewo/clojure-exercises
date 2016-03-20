17f25dcada7ec64de1632dab1b05c3a1569767c3
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-03-20 11:42:02 +0100

    Bump library versions

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [prismatic/schema "1.0.3"]
-                 [midje "1.8.2"]
+                 [prismatic/schema "1.0.5"]
+                 [midje "1.8.3"]
                  [instaparse "1.4.1"]
                  [org.clojure/data.priority-map "0.0.7"]
                  [automat "0.1.3"]
@@ -18,5 +18,5 @@
                        :repl-options {:init-ns user}
                        :plugins      [[lein-midje "3.2"]]
                        :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                      [org.clojure/java.classpath "0.2.2"]
-                                      [criterium "0.4.3"]]}})
+                                      [org.clojure/java.classpath "0.2.3"]
+                                      [criterium "0.4.4"]]}})
